df01852358bf80ead5ee164db4d840b9eb59e425
bimamib <bimaprast1601@gmail.com>
2023-09-27 06:18:59 +0700

    menerapkan metode superclass worker

--- app/src/main/java/com/bima/myworkmanager/MyWorker.kt
+++ app/src/main/java/com/bima/myworkmanager/MyWorker.kt
@@ -1,4 +1,12 @@
 package com.bima.myworkmanager
 
-class MyWorker {
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class MyWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+    override fun doWork(): Result {
+        TODO("Not yet implemented")
+    }
+
 }
