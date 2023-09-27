73dfaded396e3691f8091741132f4aad8933f543
bimamib <bimaprast1601@gmail.com>
2023-09-27 07:02:38 +0700

    menambahkan binding pada gradle, menambahkan aksi klik pada MainActivity.kt

--- app/src/main/java/com/bima/myworkmanager/MainActivity.kt
+++ app/src/main/java/com/bima/myworkmanager/MainActivity.kt
@@ -2,10 +2,24 @@ package com.bima.myworkmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.work.WorkManager
+import com.bima.myworkmanager.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var workManager: WorkManager
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        workManager = WorkManager.getInstance(this)
+        binding.btnOneTimeTask.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        TODO("Not yet implemented")
     }
 }
